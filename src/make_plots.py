d0487cf773b3dbb7ef12f26a6cf61d6b51895cba
Germán Goñi <germangoni@icloud.com>
2023-12-15 16:51:12 -0300

    Edit plot script. Add percentiles (commented by now).

--- src/make_plots.py
+++ src/make_plots.py
@@ -20,16 +20,22 @@ plt.title("Hourly temperature measurements by location")
 ax.set_xlabel("Place (Comuna)")
 ax.set_ylabel("Temperature (ºC)")
 
-# Calculate mean and standard deviation for each location
+# Calculate mean, standard deviation, 25th and 75th percentiles for each location
 means = data.groupby('lugar')['temp_celsius'].mean()
 stds = data.groupby('lugar')['temp_celsius'].std()
+p25 = data.groupby('lugar')['temp_celsius'].quantile(0.25)
+p75 = data.groupby('lugar')['temp_celsius'].quantile(0.75)
 
-# Add text annotations for mean and standard deviation
+# Add text annotations for mean, standard deviation, and percentiles
 for i, lugar in enumerate(data['lugar'].unique()):
     mean = means[lugar]
     std = stds[lugar]
     ax.text(i, mean, f"Mean: {mean:.2f}", ha='left', va='bottom')
     ax.text(i, mean + std, f"Std: {std:.2f}", ha='right', va='bottom')
+
+    # Add markers for 25th and 75th percentiles
+    # ax.plot(i, p25[lugar], marker='o', markersize=5, color='red')
+    # ax.plot(i, p75[lugar], marker='o', markersize=5, color='blue')
 
 # Save the plot as an image file
 plt.savefig(images_folder + 'stripplot.png')
